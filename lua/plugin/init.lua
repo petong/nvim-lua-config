dd19911f07905e50e589bd845b1c4506e665fcce
Peter Hicks <phicks@ripple.com>
2024-03-04 10:24:49 -0800

    fixes catpuccin plugin

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -2,7 +2,5 @@ return {
     "folke/which-key.nvim",
     "tpope/vim-fugitive",
     "NLKNguyen/papercolor-theme",
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
     "williamboman/mason.nvim"
-
 }
