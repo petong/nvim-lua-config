ef407a81f770ea6feaccca831d7794a121cd14eb
Peter Hicks <phicks@ripple.com>
2024-03-04 10:22:10 -0800

    moves luasnip to it's own plugin file

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -3,14 +3,6 @@ return {
     "tpope/vim-fugitive",
     "NLKNguyen/papercolor-theme",
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    {
-	"L3MON4D3/LuaSnip",
-        -- follow latest release.
-        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-        -- install jsregexp (optional!).
-        build = "make install_jsregexp",
-        dependencies = { "rafamadriz/friendly-snippets" },
-    },
     "williamboman/mason.nvim"
 
 }
