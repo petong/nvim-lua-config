e0969fb7af62cbafc045a811e7b8ff919690fdf4
Peter Hicks <phicks@ripple.com>
2024-03-04 10:41:30 -0800

    sets tokyonight as main colorscheme

--- lua/plugin/tokyonight.lua
+++ lua/plugin/tokyonight.lua
@@ -1,7 +1,7 @@
 return {
   {
     "folke/tokyonight.nvim",
-    lazy = false, -- make sure to load this during startup if it is your main colorscheme
+    lazy = true, -- make sure to load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require("tokyonight").setup({
