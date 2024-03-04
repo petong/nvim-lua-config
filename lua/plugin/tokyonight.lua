4646c4518d74355c4fc395e6266732c36ff6830e
Peter Hicks <phicks@ripple.com>
2024-03-04 10:41:02 -0800

    comment cleanup

--- lua/plugin/tokyonight.lua
+++ lua/plugin/tokyonight.lua
@@ -5,14 +5,9 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require("tokyonight").setup({
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
         style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
         transparent = true, -- Enable this to disable setting the background color
-        -- etc.
       })
-      -- vim.cmd.colorscheme "tokyonight"
     end,
   },
-  -- other plugins ...
 }
