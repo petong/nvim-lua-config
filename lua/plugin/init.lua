af1272f3f789fce72b7f46bf4a9cbea489c5192e
Peter Hicks <phicks@ripple.com>
2024-03-04 10:19:24 -0800

    moves nvim-tree to it's own plugin file

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -3,16 +3,6 @@ return {
     "tpope/vim-fugitive",
     "NLKNguyen/papercolor-theme",
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    {"nvim-tree/nvim-tree.lua",
-        version = "*",
-        lazy = false,
-        dependencies = {
-          "nvim-tree/nvim-web-devicons",
-        },
-        config = function()
-          require("nvim-tree").setup {}
-        end,
-    },
     {
 	"L3MON4D3/LuaSnip",
         -- follow latest release.
