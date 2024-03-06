00709c53475f443d82cc2703eddcf58ce99f32f6
Peter Hicks <phicks@ripple.com>
2024-03-05 19:32:04 -0800

    nvim-cmp now working
    we now have completion working

--- lua/plugin/nvim-cmp.lua
+++ lua/plugin/nvim-cmp.lua
@@ -1,7 +1,12 @@
 return {
     'hrsh7th/nvim-cmp',
+    lazy = false,
     dependencies = {
-      {'L3MON4D3/LuaSnip'}
+      {'L3MON4D3/LuaSnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'hrsh7th/cmp-cmdline'},
     }
 }
 
