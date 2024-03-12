52b8cb9212f9d098e8ce55d574b4dbcdbe201c94
Peter Hicks <phicks@ripple.com>
2024-03-12 12:29:05 -0700

    sets undo and backup dir

--- lua/core/set.lua
+++ lua/core/set.lua
@@ -9,8 +9,8 @@ vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.local/undodir"
+--vim.opt.backup = false
+--vim.opt.undodir = os.getenv("HOME") .. "/.local/undodir"
 vim.opt.undofile = true
 
 
@@ -27,3 +27,19 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
+
+local data_dir = vim.fn.expand('~/.local/share/nvim')
+
+-- Create data directory if it doesn't exist
+vim.fn.mkdir(data_dir, 'p')
+
+-- Set backup directory
+local backup_dir = data_dir .. '/backup//'
+vim.fn.mkdir(backup_dir, 'p')
+vim.opt.backupdir = backup_dir
+
+-- Set undo directory
+local undo_dir = data_dir .. '/undo//'
+vim.fn.mkdir(undo_dir, 'p')
+vim.opt.undodir = undo_dir
