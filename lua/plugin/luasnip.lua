7937dcca45150decf853e41c50ca1c8595515e3e
Peter Hicks <phicks@ripple.com>
2024-03-13 12:55:59 -0700

    first snippet pass

--- lua/plugin/luasnip.lua
+++ lua/plugin/luasnip.lua
@@ -5,6 +5,7 @@ return {
         version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
         -- install jsregexp (optional!).
         build = "make install_jsregexp",
-        dependencies = { "rafamadriz/friendly-snippets" },
+        dependencies = {
+            "rafamadriz/friendly-snippets" },
     },
 }
