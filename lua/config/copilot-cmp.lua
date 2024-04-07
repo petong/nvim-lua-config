ce6986db42ac9a859fa0dbb46012d399542269db
Peter Hicks <phicks@ripple.com>
2024-04-06 20:49:13 -0700

    copilot configs

---
+++ lua/config/copilot-cmp.lua
@@ -0,0 +1,10 @@
+return {
+  config = function ()
+    require("copilot_cmp").setup()
+  end,
+
+    require("copilot").setup({
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    })
+}
