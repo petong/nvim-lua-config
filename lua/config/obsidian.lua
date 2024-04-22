9c7540f8bbb725172b6840125e948b6fbb41c220
Peter Hicks <phicks@ripple.com>
2024-04-22 15:10:12 -0700

    adjust daily journal path

--- lua/config/obsidian.lua
+++ lua/config/obsidian.lua
@@ -21,7 +21,7 @@ return {
 
         daily_notes = {
           -- Optional, if you keep daily notes in a separate directory.
-          folder = "log",
+          folder = "Journal/Daily",
           -- Optional, if you want to change the date format for the ID of daily notes.
           date_format = "%Y-%m-%d",
           -- Optional, if you want to change the date format of the default alias of daily notes.
