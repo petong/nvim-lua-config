9edbd1276e9697d3049bb19cfd85b0eb32a82dc2
Peter Hicks <phicks@ripple.com>
2024-04-24 08:11:38 -0700

    comment out tab completion changes

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -41,24 +41,24 @@ cmp.setup({
         fallback()
       end
     end, {'i', 's'}),
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      local col = vim.fn.col('.') - 1
-
-      if cmp.visible() then
-        cmp.select_next_item(select_opts)
-      elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        fallback()
-      else
-        cmp.complete()
-      end
-    end, {'i', 's'}),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item(select_opts)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
+--    ['<Tab>'] = cmp.mapping(function(fallback)
+--      local col = vim.fn.col('.') - 1
+--
+--      if cmp.visible() then
+--        cmp.select_next_item(select_opts)
+--      elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+--        fallback()
+--      else
+--        cmp.complete()
+--      end
+--    end, {'i', 's'}),
+--    ['<S-Tab>'] = cmp.mapping(function(fallback)
+--      if cmp.visible() then
+--        cmp.select_prev_item(select_opts)
+--      else
+--        fallback()
+--      end
+--    end, {'i', 's'}),
   }),
   sources = cmp.config.sources({
     { name = "copilot" },
