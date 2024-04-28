cb9b7a60bcd268cbe9c53296c2a03e47cbb6e5cb
Peter Hicks <phicks@ripple.com>
2024-04-27 21:29:05 -0700

    trouble hotkeys

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -11,3 +11,11 @@ vim.keymap.set("n", "<leader><space>", ":noh<cr>")
 vim.keymap.set("n", "<leader>|", ":set cursorcolumn!<cr>")
 
 vim.keymap.set("n", "Y", "Y")
+
+-- trouble
+vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
+vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
+vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
+vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
+vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
