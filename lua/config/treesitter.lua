8703a21b83646495aeec02e1f16d7dff69b43e11
Peter Hicks <phicks@ripple.com>
2024-04-28 17:53:20 -0700

    install noice

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,7 +1,17 @@
 return {
     require'nvim-treesitter.configs'.setup {
       -- A list of parser names, or "all" (the five listed parsers should always be installed)
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+      ensure_installed = {
+          "c",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "query",
+          "regex",
+          "vim",
+          "vim",
+          "vimdoc",
+      },
 
       -- Install parsers synchronously (only applied to `ensure_installed`)
       sync_install = false,
