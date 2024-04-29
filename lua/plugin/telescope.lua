e7702c1d15bff95a87bcda2992e335ae20555f96
Peter Hicks <phicks@ripple.com>
2024-04-28 18:28:45 -0700

    comment out emoji telescope code

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -36,8 +36,8 @@ return {
                 -- argument emoji is a table.
                 -- {name="", value="", cagegory="", description=""}
 
-                vim.fn.setreg("*", emoji.value)
-                print([[Press p or "*p to paste this emoji]] .. emoji.value)
+--              vim.fn.setreg("*", emoji.value)
+--              print([[Press p or "*p to paste this emoji]] .. emoji.value)
 
                 -- insert emoji when picked
                  vim.api.nvim_put({ emoji.value }, 'c', false, true)
