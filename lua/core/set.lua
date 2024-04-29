bf54c6bb0245256b41b6147316ba59caffa192dc
Peter Hicks <phicks@ripple.com>
2024-04-28 17:54:15 -0700

    set tab stop to 2

--- lua/core/set.lua
+++ lua/core/set.lua
@@ -23,9 +23,9 @@ vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.smarttab = true
 vim.opt.shiftwidth=2
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 
