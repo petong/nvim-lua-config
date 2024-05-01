60e59855a679bc4fbfaae86c9bce2bafc814c30e
Peter Hicks <phicks@ripple.com>
2024-05-01 15:09:45 -0700

    show prompt for gen.vim

--- lua/plugin/gen-nvim.lua
+++ lua/plugin/gen-nvim.lua
@@ -19,7 +19,7 @@ return {
             -- (context property is optional).
             -- list_models = '<omitted lua function>', -- Retrieves a list of model names
             display_mode = "split", -- The display mode. Can be "float" or "split".
-            show_prompt = false, -- Shows the prompt submitted to Ollama.
+            show_prompt = true, -- Shows the prompt submitted to Ollama.
             show_model = false, -- Displays which model you are using at the beginning of your chat session.
             no_auto_close = false, -- Never closes the window automatically.
             debug = false -- Prints errors and the command which is run.
