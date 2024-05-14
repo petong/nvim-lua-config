654ff441ffdd54271d7112787244fe0941db8416
Peter Hicks <phicks@ripple.com>
2024-05-13 18:16:21 -0700

    copilot

--- lua/plugin/copilot.lua
+++ lua/plugin/copilot.lua
@@ -2,12 +2,21 @@ return {
   {
     "github/copilot.vim",
     config = function ()
-      require("copilot_cmp").setup()
+      vim.g.copilot_enabled = true
+
+      -- Configure Copilot panel
+      vim.g.copilot_panel_enabled = true
+      vim.g.copilot_panel_launch_message = true
+      vim.g.copilot_panel_default_position = "right"
+      vim.g.copilot_panel_default_width = 30
+
+      -- Configure Copilot suggestions
+      vim.g.copilot_suggestion_hidden = false
+      vim.g.copilot_suggestion_highlight_group = "CopilotSuggestion"
+      vim.cmd [[highlight CopilotSuggestion guifg=#555555 ctermfg=8]]
+
     end,
 
-    require("copilot").setup({
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-    })
   }
 }
+
