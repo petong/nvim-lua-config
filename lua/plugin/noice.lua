371977a7364a1660894c2d1de818cb255e3e5917
Peter Hicks <phicks@ripple.com>
2024-05-14 20:45:07 -0700

    fix cmdline in noice

--- lua/plugin/noice.lua
+++ lua/plugin/noice.lua
@@ -23,6 +23,18 @@ return {
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = false, -- add a border to hover docs and signature help
       },
+      views = {
+        cmdline_popup = {
+          position = {
+            row = "50%",
+            col = "50%",
+          },
+          size = {
+            width = "40%",
+            height = "auto",
+          },
+        },
+      },
     },
   }
 }
