25a4f19e2b350c65ee9cbfcb7561e82680c2a518
Peter Hicks <phicks@ripple.com>
2024-05-16 13:06:50 -0700

    delete old config

--- lua/config/luasnip.lua
+++
@@ -1,9 +0,0 @@
-return {
-    require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/lua/snippets/"}),
-    require("luasnip.loaders.from_vscode").load(),
-    require("luasnip").config.set_config({
-      history = true,
-      updateevents = "TextChanged,TextChangedI",
-    })
-
-}
