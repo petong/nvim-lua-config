20148c3b8635bf20df5572ec5790bb6d9876a653
Peter Hicks <phicks@ripple.com>
2024-05-18 12:18:17 -0700

    remove tab to select completion

--- lua/plugin/nvim-cmp.lua
+++ lua/plugin/nvim-cmp.lua
@@ -50,25 +50,21 @@ return {
         ["<C-n>"] = cmp.mapping.select_next_item(),
         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-Space>"] = cmp.mapping.complete(), -- Trigger completion with <C-Space>
         ["<C-e>"] = cmp.mapping.close(),
-        ["<CR>"] = cmp.mapping.confirm {
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
-        },
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
+        ["<C-y>"] = cmp.mapping.confirm({ -- Use <C-y> to accept completion
+          behavior = cmp.ConfirmBehavior.Insert,
+          select = false,
+        }),
+        ["<C-j>"] = cmp.mapping(function(fallback) -- Use <C-j> for snippet expansion and jumping
+          if luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
           else
             fallback()
           end
         end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
+        ["<C-k>"] = cmp.mapping(function(fallback) -- Use <C-k> for snippet jumping backwards
+          if luasnip.jumpable(-1) then
             luasnip.jump(-1)
           else
             fallback()
