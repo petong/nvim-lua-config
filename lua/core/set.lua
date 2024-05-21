70ce1f9c131760d5edfe3c882a15337f8b77b23e
Peter Hicks <phicks@ripple.com>
2024-05-20 20:27:11 -0700

    bring back del and BS from hardmode

--- lua/core/set.lua
+++ lua/core/set.lua
@@ -61,13 +61,13 @@ if hardmode then
     vim.api.nvim_set_keymap('i', '<Down>', '<C-o>' .. msg, { noremap = true, silent = false })
     vim.api.nvim_set_keymap('i', '<Left>', '<C-o>' .. msg, { noremap = true, silent = false })
     vim.api.nvim_set_keymap('i', '<Right>', '<C-o>' .. msg, { noremap = true, silent = false })
-    vim.api.nvim_set_keymap('i', '<Del>', '<C-o>' .. msg, { noremap = true, silent = false })
-    vim.api.nvim_set_keymap('i', '<BS>', '<C-o>' .. msg, { noremap = true, silent = false })
+    -- vim.api.nvim_set_keymap('i', '<Del>', '<C-o>' .. msg, { noremap = true, silent = false })
+    -- vim.api.nvim_set_keymap('i', '<BS>', '<C-o>' .. msg, { noremap = true, silent = false })
 
     -- Disable arrow keys in normal mode with a styled message
     vim.api.nvim_set_keymap('n', '<Up>', msg, { noremap = true, silent = false })
     vim.api.nvim_set_keymap('n', '<Down>', msg, { noremap = true, silent = false })
     vim.api.nvim_set_keymap('n', '<Left>', msg, { noremap = true, silent = false })
     vim.api.nvim_set_keymap('n', '<Right>', msg, { noremap = true, silent = false })
-    vim.api.nvim_set_keymap('n', '<BS>', msg, { noremap = true, silent = false })
+    -- vim.api.nvim_set_keymap('n', '<BS>', msg, { noremap = true, silent = false })
 end
