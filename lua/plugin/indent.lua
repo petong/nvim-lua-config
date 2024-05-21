5414af1bab1c8739107c6adf4422592e6e9a38a5
Peter Hicks <phicks@ripple.com>
2024-05-21 09:44:48 -0700

    keymap for indentation guides

--- lua/plugin/indent.lua
+++ lua/plugin/indent.lua
@@ -1,3 +1,11 @@
 return {
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {},
+    config = function (_, opts)
+      vim.keymap.set("n", "<leader>i", "<cmd> IBLToggle <CR>")
+      require("oil").setup(opts)
+    end,
+  }
 }
