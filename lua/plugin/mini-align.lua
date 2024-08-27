1f104407df8b158395a381c91f46133db49f2419
Peter Hicks <phicks@ripple.com>
2024-08-27 12:46:21 -0700

    leader + a for align

--- lua/plugin/mini-align.lua
+++ lua/plugin/mini-align.lua
@@ -4,8 +4,8 @@ return {
     config = function()
       require('mini.align').setup({
         mappings = {
-          start = '',
-          start_with_preview = '',
+          start = '<leader>a',
+          start_with_preview = '<leader>A',
         },
 
       })
