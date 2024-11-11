a24fc6afee76399129c63ac3c26755b48bb6e3d0
Peter Hicks <phicks@ripple.com>
2024-11-11 15:24:48 -0800

    telescope fixes merge

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -1,72 +1,107 @@
 return {
-    "nvim-telescope/telescope.nvim",
+  "nvim-telescope/telescope.nvim",
 
-    tag = "0.1.6",
+  tag = "0.1.8",
 
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope-ui-select.nvim",
-        "xiyaowong/telescope-emoji.nvim"
-    },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-ui-select.nvim",
+    "xiyaowong/telescope-emoji.nvim"
+  },
 
-    config = function()
-        require('telescope').setup({
-          extensions = {
-            ["ui-select"] = {
-              require("telescope.themes").get_dropdown {
-                -- even more opts
-              }
+  config = function()
+    local actions = require("telescope.actions")
+    local action_state = require("telescope.actions.state")
 
-              -- pseudo code / specification for writing custom displays, like the one
-              -- for "codeactions"
-              -- specific_opts = {
-              --   [kind] = {
-              --     make_indexed = function(items) -> indexed_items, width,
-              --     make_displayer = function(widths) -> displayer
-              --     make_display = function(displayer) -> function(e)
-              --     make_ordinal = function(e) -> string
-              --   },
-              --   -- for example to disable the custom builtin "codeactions" display
-              --      do the following
-              --   codeactions = false,
-              -- }
-            },
-            ["emoji"] = {
-              action = function(emoji)
-                -- argument emoji is a table.
-                -- {name="", value="", cagegory="", description=""}
+    require('telescope').setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<Tab>"] = actions.toggle_selection + actions.move_selection_next,
+            ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_previous,
+            ["<CR>"] = function(prompt_bufnr)
+              local picker = action_state.get_current_picker(prompt_bufnr)
+              local selected_entries = picker:get_multi_selection()
 
---              vim.fn.setreg("*", emoji.value)
---              print([[Press p or "*p to paste this emoji]] .. emoji.value)
+              -- If no multiple selections, perform default action
+              if #selected_entries == 0 then
+                actions.select_default(prompt_bufnr)
+                return
+              end
 
-                -- insert emoji when picked
-                 vim.api.nvim_put({ emoji.value }, 'c', false, true)
-              end,
-            }
+              -- Custom handling for multiple selections
+              actions.close(prompt_bufnr)
+              for _, entry in ipairs(selected_entries) do
+                -- Check if we're dealing with a buffer entry
+                if entry.bufnr then
+                  vim.cmd("buffer " .. entry.bufnr)
+                else
+                  vim.cmd("edit " .. entry.value)
+                end
+              end
+            end,
+          },
+          n = {
+            ["<Tab>"] = actions.toggle_selection + actions.move_selection_next,
+            ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_previous,
+            ["<CR>"] = function(prompt_bufnr)
+              local picker = action_state.get_current_picker(prompt_bufnr)
+              local selected_entries = picker:get_multi_selection()
+
+              -- If no multiple selections, perform default action
+              if #selected_entries == 0 then
+                actions.select_default(prompt_bufnr)
+                return
+              end
+
+              -- Custom handling for multiple selections
+              actions.close(prompt_bufnr)
+              for _, entry in ipairs(selected_entries) do
+                -- Check if we're dealing with a buffer entry
+                if entry.bufnr then
+                  vim.cmd("buffer " .. entry.bufnr)
+                else
+                  vim.cmd("edit " .. entry.value)
+                end
+              end
+            end,
+          },
+        },
+      },
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown {
+            -- even more opts
           }
-        })
+        },
+        ["emoji"] = {
+          action = function(emoji)
+            vim.api.nvim_put({ emoji.value }, 'c', false, true)
+          end,
+        }
+      }
+    })
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>pws', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>pWs', function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
-        vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]inf current [W]ord' })
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
-        vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F]ind [B]uffers' })
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
-        require("telescope").load_extension("ui-select")
-        require("telescope").load_extension("emoji")
-    end
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>pws', function()
+      local word = vim.fn.expand("<cword>")
+      builtin.grep_string({ search = word })
+    end)
+    vim.keymap.set('n', '<leader>pWs', function()
+      local word = vim.fn.expand("<cWORD>")
+      builtin.grep_string({ search = word })
+    end)
+    vim.keymap.set('n', '<leader>ps', function()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    end)
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
+    vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind current [W]ord' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
+    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F]ind [B]uffers' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
+    require("telescope").load_extension("ui-select")
+    require("telescope").load_extension("emoji")
+  end
 }
-
