09603e0bd7f34561190644c8fee700476bfb998b
Peter Hicks <phicks@ripple.com>
2024-11-18 14:56:28 -0800

    python treesitter changes
    Key additions and improvements:
    
    Added Python-related parsers:
    
    python for Python files
    requirements for requirements.txt files
    toml for pyproject.toml files
    
    Added textobjects configuration specifically useful for Python:
    
    Function navigation (]m, [m)
    Class navigation (]], [[)
    Parameter swapping (<leader>a, <leader>A)
    Inner/outer selections for functions, classes, loops, and parameters
    
    Added useful text objects:
    
    af/if for functions
    ac/ic for classes
    al/il for loops
    aa/ia for parameters/arguments
    
    These changes will give you:
    
    Better code navigation
    Smarter text objects for Python-specific structures
    Ability to easily swap function arguments
    Proper syntax highlighting for Python-related config files

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -1,43 +1,17 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   event = { "BufReadPre", "BufNewFile" },
-  dependencies = {"nvim-treesitter/nvim-treesitter-textobjects"},
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    "nvim-treesitter/playground",
+  },
   build = ":TSUpdate",
   opts = {
-    -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
-
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
     auto_install = true,
-
-    -- List of parsers to ignore installing (or "all")
     ignore_install = { "javascript" },
-
-    ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
     highlight = {
       enable = true,
-
-      -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-      -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-      -- the name of the parser)
-      -- list of language that will be disabled
-      -- disable = { "c", "rust" },
-      -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
---      disable = function(lang, buf)
---        local max_filesize = 100 * 1024 -- 100 KB
---        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
---        if ok and stats and stats.size > max_filesize then
---          return true
---        end
---      end,
-
-      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-      -- Using this option may slow down your editor, and you may see some duplicate highlights.
-      -- Instead of true it can also be a list of languages
       additional_vim_regex_highlighting = false,
     },
     indent = { enable = true },
@@ -47,22 +21,76 @@ return {
       "lua",
       "markdown",
       "markdown_inline",
+      "python",  -- Add Python parser
       "query",
       "regex",
       "vim",
       "vimdoc",
+      "requirements", -- For requirements.txt files
+      "toml",    -- For pyproject.toml files
     },
     incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    },
+    -- Add textobjects configuration for Python
+    textobjects = {
+      select = {
         enable = true,
+        lookahead = true,
         keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
+          -- You can use the capture groups defined in textobjects.scm
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner",
+          ["al"] = "@loop.outer",
+          ["il"] = "@loop.inner",
+          ["aa"] = "@parameter.outer",
+          ["ia"] = "@parameter.inner",
+          ["is"] = "@statement.inner",
+          ["as"] = "@statement.outer",
         },
+      },
+      -- Enable this for moving between functions/classes
+      move = {
+        enable = true,
+        set_jumps = true,
+        goto_next_start = {
+          ["]m"] = "@function.outer",
+          ["]]"] = "@class.outer",
+        },
+        goto_next_end = {
+          ["]M"] = "@function.outer",
+          ["]["] = "@class.outer",
+        },
+        goto_previous_start = {
+          ["[m"] = "@function.outer",
+          ["[["] = "@class.outer",
+        },
+        goto_previous_end = {
+          ["[M"] = "@function.outer",
+          ["[]"] = "@class.outer",
+        },
+      },
+      -- Enable this for swapping arguments/parameters
+      swap = {
+        enable = true,
+        swap_next = {
+          ["<leader>a"] = "@parameter.inner",
+        },
+        swap_previous = {
+          ["<leader>A"] = "@parameter.inner",
+        },
+      },
     },
   },
-  config = function (_, opts)
+  config = function(_, opts)
     local configs = require("nvim-treesitter.configs")
     configs.setup(opts)
   end
